b4b376595c84f76eced9a9ade5e7b572d4cf2c4f
enisruyaa@gmail.com <enisruyaa@gmail.com>
2026-02-28 20:46:23 +0300

    Add project files.

---
+++ PingCheck/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace PingCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbResult = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ENTER IP ADDRESS";
+            // 
+            // txtIp
+            // 
+            this.txtIp.AcceptsReturn = true;
+            this.txtIp.Location = new System.Drawing.Point(13, 30);
+            this.txtIp.Multiline = true;
+            this.txtIp.Name = "txtIp";
+            this.txtIp.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtIp.Size = new System.Drawing.Size(149, 299);
+            this.txtIp.TabIndex = 1;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(183, 151);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(185, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ENTER PORT";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(199, 177);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 4;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(199, 206);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Location = new System.Drawing.Point(289, 30);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(276, 299);
+            this.rtbResult.TabIndex = 6;
+            this.rtbResult.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(378, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "RESULT :";
+            // 
+            // cmbResult
+            // 
+            this.cmbResult.FormattingEnabled = true;
+            this.cmbResult.Location = new System.Drawing.Point(447, 8);
+            this.cmbResult.Name = "cmbResult";
+            this.cmbResult.Size = new System.Drawing.Size(118, 21);
+            this.cmbResult.TabIndex = 8;
+            this.cmbResult.SelectedIndexChanged += new System.EventHandler(this.cmbResult_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(577, 341);
+            this.Controls.Add(this.cmbResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rtbResult);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Enis RÜYA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbResult;
+    }
+}
+
---
+++ PingCheck/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> allResult = new List<string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbResult.Items.Add("----------");
+            cmbResult.Items.Add("All");
+            cmbResult.Items.Add("Success");
+            cmbResult.Items.Add("Failed");
+            cmbResult.SelectedIndex = 0;
+        }
+
+        private void WriteLog(string message,Color color)
+        {
+            allResult.Add(message);
+            rtbResult.SelectionStart = rtbResult.TextLength;
+            rtbResult.SelectionLength = 0;
+            rtbResult.SelectionColor = color;
+            rtbResult.AppendText(message + Environment.NewLine);
+
+            rtbResult.SelectionColor = rtbResult.ForeColor;
+        }
+
+        private void FillList(string filter)
+        {
+            rtbResult.Clear();
+
+            foreach (string item in allResult)
+            {
+                if (filter == "All" ||
+                    (filter == "Success" && item.Contains("Success")) ||
+                    (filter == "Failed" && item.Contains("Failed")))
+                {
+                    Color renk;
+
+                    if (item.Contains("Ping: Failed"))
+                        renk = Color.Red;
+                    else
+                        renk = Color.Green;
+
+                    rtbResult.SelectionStart = rtbResult.TextLength;
+                    rtbResult.SelectionLength = 0;
+                    rtbResult.SelectionColor = renk;
+                    rtbResult.AppendText(item + Environment.NewLine);
+                    rtbResult.SelectionColor = rtbResult.ForeColor;
+                }
+            }
+        }
+
+        private async Task<bool> PingCheck(string ip)
+        {
+            try
+            {
+                using (Ping ping = new Ping())
+                {
+                    PingReply reply = await ping.SendPingAsync(ip, 2000);
+                    return reply.Status == IPStatus.Success;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private async Task<bool> PortCheck(string ip, int port)
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    Task connectTask = client.ConnectAsync(ip, port);
+                    Task timeoutTask = Task.Delay(2000);
+
+                    Task completed = await Task.WhenAny(connectTask, timeoutTask);
+
+                    return completed == connectTask && client.Connected;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void cmbResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillList(cmbResult.Text);
+        }
+
+        private async void btnCheck_Click(object sender, EventArgs e)
+        {
+            allResult.Clear();
+            rtbResult.Clear();
+
+            string[] ipList = txtIp.Text.Split(
+                new[] { Environment.NewLine },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            if (ipList.Length == 0)
+            {
+                MessageBox.Show("En az bir IP adresi giriniz.");
+                return;
+            }
+
+            bool portGirildi = int.TryParse(txtPort.Text, out int port);
+
+            foreach (string ip in ipList)
+            {
+                string temizIP = ip.Trim();
+
+                bool pingBasarili = await PingCheck(temizIP);
+
+                string pingSonuc = pingBasarili
+                    ? "Ping: Success"
+                    : "Ping: Failed";
+
+                string portSonuc = "";
+
+                if (portGirildi)
+                {
+                    bool portAcik = await PortCheck(temizIP, port);
+
+                    portSonuc = portAcik
+                        ? $" | Port {port}: Open"
+                        : $" | Port {port}: Close";
+                }
+
+                string finalSatir = $"{temizIP} | {pingSonuc}{portSonuc}";
+
+                allResult.Add(finalSatir);
+            }
+
+            cmbResult.SelectedIndex = 1;
+            FillList("All");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            allResult.Clear();
+            rtbResult.Clear();
+            txtPort.Clear();
+            txtIp.Clear();
+            cmbResult.SelectedIndex = 0;
+        }
+    }
+}
